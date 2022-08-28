c5c3fab0be3a4dc5f9fc0f703dbd08ec80f99c86
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-28 20:42:41 +0200

    added jellyfin

--- containers.sh
+++ containers.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-PATH_TO_SCRIPTS="$HOME/bash-debian-server"
-PATH_TO_SERVER_DATA= $HOME/truenas/Config/server_data
+PATH_TO_SCRIPTS = "$HOME/bash-debian-server"
+PATH_TO_SERVER_DATA = $HOME/truenas/Config/server_data
+PATH_TO_SERVER_MEDIA = $HOME/truenas/truenas/Public/Torrent
+
+PATH_TO_JELLYFIN = $HOME/truenas/jellyfin
 
 #Installing portainer
 cd $PATH_TO_SCRIPTS
 mkdir $PATH_TO_SERVER_DATA/portainer_data
 cd portainer
+docker-compose up -d
+
+#Installing jellyfin
+cd $PATH_TO_SCRIPTS
+mkdir $PATH_TO_SERVER_DATA/jellyfin
+mkdir $PATH_TO_SERVER_DATA/jellyfin/jellyfin_cache
+mkdir $PATH_TO_SERVER_DATA/jellyfin/jellyfin_config
+cd jellyfin
 docker-compose up -d
